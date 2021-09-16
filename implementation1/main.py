77e6171892e9ea50c05b20484eed998ee184790a
Alperen Ağa <alperencode@gmail.com>
2021-09-16 13:52:44 +0300

    Update main.py day 3 or 4

--- implementation1/main.py
+++ implementation1/main.py
@@ -14,16 +14,36 @@ url = requests.get("https://www.serversupply.com/HARD%20DRIVES")
 
 soup = BeautifulSoup(url.content, "lxml")
 
-Categories = soup.find_all("a",class_="dropdown-item")
+ctg = soup.find_all("a",class_="dropdown-item")
+ctglist = []
 
-for i in Categories:
+for i in ctg:
     print(i.text)
-    data_dict['category'] = i.text
+    if(i.get('href') == "/Register" or i.text == "Forgot password?"):
+        pass
+    else:
+        ctglist.append(i.text)
+
+    # data_dict['category'] = i.text
+    # masterlist.append(data_dict)
+
+print(ctglist)
+
+# print(data_dict)
+    
+
+
+# Will add: i need to create a column named Category and add the ctglist values as a line
+# For now if i try to add them as a line its only adding the last value for all lines
+# for example:
+
+for z in ctglist:
+    data_dict['category'] = z
     masterlist.append(data_dict)
 
 # print(masterlist)
 df = pd.DataFrame(masterlist)
 print(df)
 
-# Here is the bug === its adding only last category to master list
-# And its gathering first two menu which is not category
+# so for now i can gather the data but cant list for pandas table
+# and to output as excel file i need to use pandas
