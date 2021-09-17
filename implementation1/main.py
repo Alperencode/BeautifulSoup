77bd780c3625b3cb856d826765f29566be080647
Alperen Ağa <alperencode@gmail.com>
2021-09-17 10:09:45 +0300

    main.py day4 update

--- implementation1/main.py
+++ implementation1/main.py
@@ -10,40 +10,27 @@ data_dict = {}
 # df = pd.DataFrame(masterlist) # to see datas
 # df.to_csv('Name') # to excel
 
-url = requests.get("https://www.serversupply.com/HARD%20DRIVES")
-
+# Gathering Server Data
+url = requests.get("https://www.serversupply.com")
 soup = BeautifulSoup(url.content, "lxml")
+# ------------------------------------
 
-ctg = soup.find_all("a",class_="dropdown-item")
-ctglist = []
 
-for i in ctg:
-    print(i.text)
-    if(i.get('href') == "/Register" or i.text == "Forgot password?"):
-        pass
+ctg = soup.find_all("li")
+
+for i in ctg[6].next_siblings:
+    if i.text.strip() == "Networking":
+        data_dict['Sub Category'] = i.text.strip()
+        print(i.text.strip())
     else:
-        ctglist.append(i.text)
-
-    # data_dict['category'] = i.text
-    # masterlist.append(data_dict)
-
-print(ctglist)
-
-# print(data_dict)
-    
-
-
-# Will add: i need to create a column named Category and add the ctglist values as a line
-# For now if i try to add them as a line its only adding the last value for all lines
-# for example:
-
-for z in ctglist:
-    data_dict['category'] = z
-    masterlist.append(data_dict)
+        data_dict['Category'] = i.text.strip()
+        print(i.text.strip())
+  
+print(data_dict)
 
 # print(masterlist)
 df = pd.DataFrame(masterlist)
-print(df)
+#print(df)
 
 # so for now i can gather the data but cant list for pandas table
 # and to output as excel file i need to use pandas
