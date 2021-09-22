58cfb2fb631b9cb4adcb2405eb86f1cb0ee9078a
Alperen Ağa <alperencode@gmail.com>
2021-09-22 14:51:47 +0300

    Add implementation2

--- implementation2/main.py
+++ implementation2/main.py
@@ -1,1 +1,15 @@
-import bs4
+from bs4 import BeautifulSoup
+import requests
+url = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=")
+
+
+soup = BeautifulSoup(url.content,"lxml")
+
+jobs = soup.find_all("li",class_="clearfix job-bx wht-shd-bx")
+
+for job in jobs:
+    companyName = job.find("h3",class_="joblist-comp-name").text.strip()
+    skills = job.find("span", class_="srp-skills").text.strip().replace(" ","")
+    print(f"Company Name: {companyName}")
+    print(f"Skills: {skills}")
+    print("")
