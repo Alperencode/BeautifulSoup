0030f518e533336bac676c076823196bf3362a40
Alperen Ağa <alperencode@gmail.com>
2021-10-06 23:19:25 +0300

    Update implementation5 for new Upwork Job

--- implementation5/main.py
+++ implementation5/main.py
@@ -1,7 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = requests.get("")
+# gather the tables to excel
+
+url = requests.get("https://www.nfl.com/stats/team-stats/offense/passing/2021/reg/all")
 soup = BeautifulSoup(url.content,"lxml")
 
-print(soup)
+names = soup.find_all("div",class_="d3-o-club-fullname")
+
+# for name in names:
+#     print(name.text.strip())
+
+table = soup.find("table").tbody
+teams = table.tr.next_siblings
+
+for i in teams:
+    try:
+        print(i.td)
+    except:
+        pass
+
