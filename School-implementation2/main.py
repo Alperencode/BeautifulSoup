31035be0d105c7df85d8640a6c6c8ea73939fd78
Alperen Aga <alperencode@gmail.com>
2021-11-02 12:00:53 +0300

    Update School-implementation2 main.py :wrench:

--- School-implementation2/main.py
+++ School-implementation2/main.py
@@ -4,9 +4,11 @@ from bs4 import BeautifulSoup
 session = requests.Session()
 
 jar = requests.cookies.RequestsCookieJar()
-jar.set('JSESSIONID','jeiAQ9d3OK7OBGvJrpg3nphOIRm_NGFnsSnb4QSa.jbossn182:TEZ_8150"; TS01e18b4b=01026844b867339fd2b4a5c28af84150beba29cd8fd381ac9747fb6f7d9d2e78d26e3547531fdca0a0d61901ea75f957e2ba4991201e0cc90a1d4940a8931e4c61c444ed4d; _ga=GA1.3.1606619693.1634755601; _gid=GA1.3.1848535403.1635773551; TS014c3a3f=01026844b87acc3f3acc0f060c038eb9bec32b8b5e25d375734343c195519a076e175618edbf90f84200f91e19726d5746b8247d50; TScee0d053027=0868d5a3ffab2000ad846b42eda896022658d6bfc405cbb38fbacabe905156d8e0ed15a50f14ed8a0867405acd113000b08fdc5440bd47c6f2b9a2cd12fb3cf20edd5186a8a91a289870e720caf5c1a4a00fbe650a3a68003d2cdbf4f58930b2; _gat=1')
+jar.set('JSESSIONID','"R6BD0hbICZ2gGiLeqvjhlO-kFP2Ko3y4i3Rxk2jX.jbossn183:TEZ_8150"; TS01e18b4b=01026844b842721713e4bf5b2fff46eb8572168e0ddb424608cfaaa87a20f40c847056a6f101079411d0457c55b06c0f463672ee6116b3f36b3cfc24ed565c005fb42d5040; _ga=GA1.3.1694449284.1635759066; _gid=GA1.3.1885659291.1635759066; _gat=1; TS014c3a3f=01026844b87977383618aa452990ea860125b94583bb4f2532334fc5ff218cd61bf1cfee0705eca67d30d3b01931628ed04ebab2fe; TScee0d053027=0868d5a3ffab20009499254fe5bfc63e0f684b690cc763aafa9fbc0373d8bef64791b04ae8d599d208efbfff00113000c7aac62fd4c1f39e15169939df45fed3987ec7ebaab22dd5ecc127b2345044f7978760407b786dc912a0fbcbeeab0d33')
 
 session.cookies = jar
 
-url = session.post('https://tez.yok.gov.tr/UlusalTezMerkezi/SearchTez')
-print(url.text)
+url = session.get('https://tez.yok.gov.tr/UlusalTezMerkezi/tezSorguSonucYeni.jsp')
+soup = BeautifulSoup(url.content, "lxml")
+
+print(soup)
