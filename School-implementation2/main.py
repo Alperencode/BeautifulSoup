dd20827749eaede7255d5c41cf905ab527d54208
Alperen Aga <alperencode@gmail.com>
2021-11-11 03:08:53 +0300

    Update School-implementation2 main.py :wrench:

--- School-implementation2/main.py
+++ School-implementation2/main.py
@@ -1,3 +1,4 @@
+from requests.structures import CaseInsensitiveDict
 import requests
 from bs4 import BeautifulSoup
 
@@ -24,6 +25,18 @@ payload = {
         }
 
 url = session.post('https://tez.yok.gov.tr/UlusalTezMerkezi/SearchTez',data=payload)
+headers = CaseInsensitiveDict()
+headers["Cookie"] =  'JSESSIONID="Cy0gHV4QN6DjdtbMEhPA5LlR4nIVwSo8z08H8RoV.jbossn183:TEZ_8150"';"TS01e18b4b=01026844b8c67d161539dfd8f5dd42d826cefa534f246868374012faf21e9e808d265c9a84f883a0f5fd8c3c097c6980d447638982213fdf0889e4bcc245ff69a7b98cb035";"TS014c3a3f=01026844b832c2fe02e63442694aa776005d952617246868374012faf21e9e808d265c9a848b2fd624a0bf5034cf8eb36ee901369a"
+
+
+cookieValue = url.cookies.items()[1][1].replace('"',"")
+print(url.cookies.items())
+
 print(url.cookies)
-soup = BeautifulSoup(url.content,"lxml")
+print(url.cookies.items()[0])
+url2 = session.get("https://tez.yok.gov.tr/UlusalTezMerkezi/tezSorguSonucYeni.jsp",headers = headers)
+soup = BeautifulSoup(url2.content,"lxml")
 print(soup)
+# print(url.cookies)
+# print(url.cookies.items()[1][1])
+# url2 = requests.get("https://tez.yok.gov.tr/UlusalTezMerkezi/tezSorguSonucYeni.jsp",)
