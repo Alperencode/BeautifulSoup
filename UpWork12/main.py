42a3da08efa91ee7e990a9a196651c130454d396
Alperen Aga <alperencode@gmail.com>
2023-03-25 19:08:14 +0300

    Update UpWork12 Clean the code and add SafeFind() function

--- UpWork12/main.py
+++ UpWork12/main.py
@@ -1,18 +1,21 @@
 import requests, csv
 from bs4 import BeautifulSoup
 
+# Variables
 requestCounter, errorCounter, sessionCounter, parseCounter = 0, 0, 0, 0
-imageURLS, masterList = [], []
+imageURLS = []
 MAX_REQUEST = 100
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
 }
+
 
 # Initials
 session = requests.Session()
 with open("output.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(["URL", "Title", "Sale Price", "Price", "SKU", "Category", "Brand", "Image URL", "Description Short", "Description Long"])
+
 
 # Functions
 def ChangeSession():
@@ -61,80 +64,46 @@ def SaveImage(url, name):
     with open(f"images/{name}.jpg", "wb") as f:
         f.write(img.content)
     
+def SafeFind(soup, tag, class_=None, id=None):
+    """Helper function to safely find element in soup"""
+    if id:
+        result = soup.find(tag, id=id)
+    else:
+        result = soup.find(tag, class_=class_)
+    return result.text.strip() if result else ""
+
 def ParseProductPage(url):
-    global imageURLS
+    """ Parses the product page and writes to CSV """
+    global imageURLS, parseCounter
     dataDict = {}
-    global parseCounter
     parseCounter += 1
     soup = SendRequest(url)
 
+    # Product Info
     dataDict['Product URL'] = url
+    dataDict['Product Title'] = SafeFind(soup, tag="h1", class_="product_title entry-title")
+    dataDict['Product Description Short'] = SafeFind(soup, tag="div", class_="woocommerce-product-details__short-description")
+    dataDict['Product Description Long'] = SafeFind(soup, tag="div", id="cgkit-tab-description")
+    dataDict['SKU'] = SafeFind(soup, tag="span", class_="sku")
 
-    try:
-        productTitle = soup.find("h1",class_="product_title entry-title").text.strip()
-    except:
-        productTitle = ""
-    dataDict['Product Title'] = productTitle
+    # Product Price
+    productPrices = SafeFind(soup, tag="p", selector="price")
+    if productPrices:
+        dataDict['Product Price'] = productPrices.findAll("del")[2].text.strip()
+        dataDict['Product Sale Price'] = productPrices.find("ins").text.strip()
 
-    try:
-        productDescriptionShort = soup.find("div",class_="woocommerce-product-details__short-description").text.strip()
-    except:
-        productDescriptionShort = ""
-    dataDict['Product Description Short'] = productDescriptionShort
+    # Category & Brand
+    postedIn = SafeFind(soup, tag="span", class_="posted_in")
+    if postedIn:
+        dataDict['Category'] = postedIn[0].text.strip()
+        dataDict['Brand'] = postedIn[1].text.strip()
 
-    try:
-        productDescriptionLong = soup.find("div", id="cgkit-tab-description").text.strip()
-    except:
-        productDescriptionLong = ""
-    dataDict['Product Description Long'] = productDescriptionLong
-
-    try:
-        productPrices = soup.find("p",class_="price")
-    except:
-        productPrices = ""
-
-    try:
-        productPrice = soup.findAll("del")[2].text.strip()
-    except:
-        productPrice = ""
-    dataDict['Product Price'] = productPrice
-
-    try:
-        productSalePrice = productPrices.ins.text.strip()
-    except:
-        productSalePrice = ""
-    dataDict['Product Sale Price'] = productSalePrice
-
-    try:
-        sku = soup.find("span",class_="sku").text.strip()
-    except:
-        sku = ""
-    dataDict['SKU'] = sku
-
-    try:
-        postedIn = soup.findAll("span",class_="posted_in")
-    except:
-        postedIn = ""
-
-    try:
-        category = postedIn[0].text.strip()
-    except:
-        category = ""
-    dataDict['Category'] = category
-
-    try:
-        brand = postedIn[1].text.strip()
-    except:
-        brand = ""
-    dataDict['Brand'] = brand
-
-    try:
-        imageUrl = soup.find("figure",class_="woocommerce-product-gallery__wrapper").img.get('src')
-        imageURLS.append(imageUrl)
-    except:
-        imageUrl = ""
+    # Image URL
+    imageUrl = soup.find("figure",class_="woocommerce-product-gallery__wrapper").img.get('src')
+    imageURLS.append(imageUrl)
     dataDict['Image URL'] = imageUrl
 
+    # Write to CSV
     with open("output.csv", "a", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow([
@@ -150,13 +119,16 @@ def ParseProductPage(url):
             dataDict['Product Description Long']
         ])
 
-    return dataDict
 
 # Loops
 for pageNumber in range(1, GetMaxPage()+1):
     url = f"https://service-workshopmanual.com/product-category/machine-vehicle-manuals/page/{pageNumber}"
     for url in ScrapeURL(url):
-        masterList.append(ParseProductPage(url))
+        try:
+            ParseProductPage(url)
+        except:
+            errorCounter += 1
+            print(f"Error: {url}")
 
 for image in imageURLS:
     SaveImage(image, name=imageURLS.index(image)+1)
